1c757edeb2800cc1a6cd8b5d57db79b13bcf7bf1
Sean Oliver <soliver87@gmail.com>
2011-08-19 16:26:45 -0700

    light refactoring and initial blueprint css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Return a title on a per-page basis. And provide a generic title if no @title is specified.
+  def title
+    base_title = "SO Rails App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
 end
